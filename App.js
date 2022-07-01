cea4e6d1360d383b6d1058c18e73533a7fb3c7e7
Vince Ding <vince.ding@gmail.com>
2022-07-01 19:53:44 +0800

    version 1

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View, SafeAreaView, } from 'react-native';
+import { names } from "./names";
 
-export default function App() {
+//
+const namesAsObjects = names.map((item) => {
+  return { name: item};
+ });
+ 
+ //
+ export default function App() {
+  const renderName = ({ item }) => {
+    return <Text style={[styles.textStyle]}>{item.name}</Text>;
+  };
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FlatList
+      data={namesAsObjects}
+      renderItem={renderName}
+      keyExtractor={(item) => item.name}
+    />
   );
-}
+ }
 
+
+//Styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+
+  textStyle: {
+    fontSize: 18,
+    color: 'white',
+    margin: 10,
+    padding: 10,
+    width: 300,
+    textAlign: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'purple'
+    
+    
+  }
+
+ });
+
+
+
+
